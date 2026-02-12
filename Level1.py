b9bc3564b61b2e70c7b9feb7b99ee036c5578633
Muhammad Rizky Adistiya <rizkyadistiya4@gmail.com>
2026-02-12 11:36:53 +0700

    Python 1 Tipe Data

---
+++ Level1.py
@@ -0,0 +1,17 @@
+#Jenis jenis tipe data dalam python
+
+#Ini adalah komentar
+
+#contoh dengan membuat data karakter dalam game
+
+nama_hero = "Arch Mage" #String
+level = 99 #Integer
+power = 99.5 #Floating
+is_active = True #Boolean
+
+#Mari kita tampilkan
+print("---STATUS PEMAIN---")
+print("Nama hero: ", nama_hero)
+print("Level: ",level)
+print("Power: ,", power)
+print("Status Aktif: ", is_active)
